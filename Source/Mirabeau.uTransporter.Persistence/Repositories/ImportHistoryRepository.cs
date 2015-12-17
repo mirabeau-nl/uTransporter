4b5a9fbc5ae756cc9f48f3617683513fc1f82a95
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2015-12-17 16:17:38 +0100

    added import history repository

---
+++ Source/Mirabeau.uTransporter.Persistence/Repositories/ImportHistoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Mirabeau.uTransporter.Persistence.Providers;
+
+namespace Mirabeau.uTransporter.Persistence.Repositories
+{
+    public class ImportHistoryRepository : IImportHistoryRepository
+    {
+        private readonly IImportHistoryUnitOfWorkProvider _unitOfWorkProvider;
+
+        public ImportHistoryRepository(IImportHistoryUnitOfWorkProvider unitOfWorkProvider)
+        {
+            _unitOfWorkProvider = unitOfWorkProvider;
+        }
+    }
+
+    public interface IImportHistoryRepository
+    {
+
+    }
+}
