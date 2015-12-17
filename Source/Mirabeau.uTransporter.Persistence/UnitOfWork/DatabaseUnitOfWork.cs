f29ab19bec5eb3e665d70c2c469d6fb342b7e223
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2015-12-17 15:28:13 +0100

    created persistence project

---
+++ Source/Mirabeau.uTransporter.Persistence/UnitOfWork/DatabaseUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core.Persistence;
+
+namespace Mirabeau.uTransporter.Persistence.UnitOfWork
+{
+    public class DatabaseUnitOfWork
+    {
+        public DatabaseUnitOfWork(string connectionString)
+        {
+            Database = new Database(connectionString, "System.Data.SqlClient");
+        }
+
+        public Database Database { get; set; }
+    }
+}
